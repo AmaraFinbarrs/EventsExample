f4e02a53bcf0133fb02b2c584d053de7f8b29555
Amara Finbarrs <100963912+AmaraFinbarrs@users.noreply.github.com>
2022-04-30 07:46:30 +0100

    Add project files.

---
+++ ClassLibrary1/Publisher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace ClassLibrary1
+{
+    //delegate type
+    public delegate void MyDelegateType(int a, int b);
+    
+    //Publisher
+    public class Publisher
+    {
+        //step 1: create event
+        public event MyDelegateType myEvent;
+
+        public void RaiseEvent(int a, int b)
+        {
+            //step 2: raise event
+            if (this.myEvent != null)
+            {
+                this.myEvent(a, b);
+            }
+        }
+    }
+}
---
+++ EventsExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace EventsExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create obj of Subscriber class
+            Subscriber subscriber = new Subscriber();
+
+            //create obj of Publisher class
+            Publisher publisher = new Publisher();
+
+            //handle the event or subscribe to event
+            publisher.myEvent += subscriber.Add;
+
+            //invoke the event
+            publisher.RaiseEvent(10, 20);
+            publisher.RaiseEvent(50, 20);
+            publisher.RaiseEvent(900, 20);
+
+            Console.ReadKey();
+        }
+    }
+}
